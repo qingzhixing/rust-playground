dd39941f95b5efd44fa2675cdc1abcd6fbb8f1ff
qingzhixing <qzsqqmail@qq.com>
2026-03-11 17:50:56 +0800

    📦 NEW: Adder

---
+++ adder/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(4, result);
+    }
+}
