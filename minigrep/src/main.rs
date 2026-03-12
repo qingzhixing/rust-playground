9bffb895091f3ef430e429a18c978e1f81f500c6
qingzhixing <qzsqqmail@qq.com>
2026-03-12 09:44:29 +0800

    📦 NEW: 添加文件读取的功能

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
-    let filename = &args[2];
+    let file_path = &args[2];
 
-    println!("query: {}, filename: {}", query, filename);
+    println!("query: \"{query}\", filename: \"{file_path}\"");
+
+    let content = fs::read_to_string(file_path).expect("should have been able to file content");
+
+    println!("content: \n{content}");
 }
