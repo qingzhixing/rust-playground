1457eb603faef30afe72c3e844b42fc9757e132b
qingzhixing <qzsqqmail@qq.com>
2026-04-09 19:00:03 +0800

    📦 NEW: handle_key_event

--- ratatui-counter-app/src/main.rs
+++ ratatui-counter-app/src/main.rs
@@ -74,6 +74,27 @@ impl App {
         };
         Ok(())
     }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
 }
 
 #[cfg(test)]
