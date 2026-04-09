f92855b3f3bf7c5efd9c21aa4fa5ac97fd53868c
qingzhixing <qzsqqmail@qq.com>
2026-04-09 21:13:48 +0800

    🎨 DESIGN: UI render chunks

--- ratatui-json-editor/src/ui.rs
+++ ratatui-json-editor/src/ui.rs
@@ -1,7 +1,7 @@
 use ratatui::{
     Frame,
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     widgets::{Paragraph, Widget},
 };
 
@@ -22,8 +22,36 @@ impl Ui {
 
 impl Widget for &Ui {
     fn render(self, area: Rect, buffer: &mut Buffer) {
-        Paragraph::new("Hello, world!")
-            .centered()
-            .render(area, buffer);
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Length(3),
+                Constraint::Min(1),
+                Constraint::Length(3),
+            ])
+            .split(area);
     }
+}
+
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
 }
