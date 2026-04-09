198fc908ca6f8d8d5966477a6f1586683cb0663f
qingzhixing <qzsqqmail@qq.com>
2026-04-09 20:22:08 +0800

    📦 NEW: app.rs

--- ratatui-json-editor/src/app.rs
+++ ratatui-json-editor/src/app.rs
@@ -16,3 +16,41 @@ pub struct App {
     pub current_screen: CurrentScreen, // the current screen the user is looking at, and will later determine what is rendered.
     pub currently_editing: Option<CurrentEditing>, // the optional state containing which of the key or value pair the user is editing. It is an option, because when the user is not directly editing a key-value pair, this will be set to `None`.
 }
+
+impl App {
+    pub fn new() -> Self {
+        Self {
+            key_input: String::new(),
+            value_input: String::new(),
+            pairs: HashMap::new(),
+            current_screen: CurrentScreen::Main,
+            currently_editing: None,
+        }
+    }
+
+    pub fn save_key_value(&mut self) {
+        self.pairs
+            .insert(self.key_input.clone(), self.value_input.clone());
+
+        self.key_input = String::new();
+        self.value_input = String::new();
+        self.currently_editing = None;
+    }
+
+    pub fn toggle_editing(&mut self) {
+        if let Some(edit_mode) = self.currently_editing {
+            self.currently_editing = match edit_mode {
+                CurrentEditing::Key => Some(CurrentEditing::Value),
+                CurrentEditing::Value => Some(CurrentEditing::Key),
+            };
+        } else {
+            self.currently_editing = Some(CurrentEditing::Key);
+        }
+    }
+
+    pub fn print_json(&self) -> color_eyre::Result<()> {
+        let output = serde_json::to_string_pretty(&self.pairs)?;
+        println!("{output}");
+        Ok(())
+    }
+}
